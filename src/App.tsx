a9467eedc3d1e961ad5916f33bc439e000b8fa94
tcocco20 <theo@coccoscoding.dev>
2024-07-05 15:19:20 -0700

    set up new app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@nextui-org/react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl">React TypeScript App</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
