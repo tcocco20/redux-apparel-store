00d12764c0d2a0ecc3c317bf10fae9f91634adbe
tcocco20 <theo@coccoscoding.dev>
2024-07-20 12:11:23 -0700

    got cart modal to show up

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,23 +1,31 @@
-import { createPortal } from 'react-dom';
-
-import CartItems from './CartItems.tsx';
+import { Modal, ModalBody, ModalContent } from "@nextui-org/react";
+import CartItems from "./CartItems.tsx";
 
 type CartProps = {
   onClose: () => void;
 };
 
 export default function Cart({ onClose }: CartProps) {
-  return createPortal(
-    <>
-      <div className="cart-backdrop" />
-      <dialog id="cart-modal" open>
-        <h2>Your Cart</h2>
-        <CartItems />
-        <p id="cart-actions">
-          <button onClick={onClose}>Close</button>
-        </p>
-      </dialog>
-    </>,
-    document.getElementById('modal')!
+  return (
+    <Modal
+      isOpen={true}
+      onOpenChange={onClose}
+      placement="top"
+      closeButton={<div className="hidden pointer-events-none" />}
+    >
+      <ModalContent>
+        {(onClose) => (
+          <ModalBody>
+            <div id="cart-modal">
+              <h2 className="text-2xl font-serif">Your Cart</h2>
+              <CartItems />
+              <p id="cart-actions">
+                <button onClick={onClose}>Close</button>
+              </p>
+            </div>
+          </ModalBody>
+        )}
+      </ModalContent>
+    </Modal>
   );
 }
