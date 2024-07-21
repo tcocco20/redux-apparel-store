6e8f85669d383372f81ad806868020161079e7a5
tcocco20 <theo@coccoscoding.dev>
2024-07-21 11:26:56 -0700

    set up reducer for redux store

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,28 @@ import Header from "./components/Header.tsx";
 import Shop from "./components/Shop.tsx";
 import Product from "./components/Product.tsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.ts";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
 function App() {
   return (
-    <main className="bg-yellow-950">
-      <div className="container mx-auto px-40 pb-8">
-        <Header />
-        <Shop>
-          {DUMMY_PRODUCTS.map((product) => (
-            <li
-              className="w-[calc(33%-18px)] bg-stone-500/50 rounded-lg shadow-2xl max-h-[672px]"
-              key={product.id}
-            >
-              <Product {...product} />
-            </li>
-          ))}
-        </Shop>
-      </div>
-    </main>
+    <Provider store={store}>
+      <main className="bg-yellow-950">
+        <div className="container mx-auto px-40 pb-8">
+          <Header />
+          <Shop>
+            {DUMMY_PRODUCTS.map((product) => (
+              <li
+                className="w-[calc(33%-18px)] bg-stone-500/50 rounded-lg shadow-2xl max-h-[672px]"
+                key={product.id}
+              >
+                <Product {...product} />
+              </li>
+            ))}
+          </Shop>
+        </div>
+      </main>
+    </Provider>
   );
 }
 
